bd71b30a79268229924b4b6f7da78df1ed7fb48c
branden <branden.bowling@sinclair.edu>
2024-07-28 23:15:21 -0400

    2

--- part a/main.cpp
+++ part a/main.cpp
@@ -11,7 +11,19 @@ using namespace std;
 class invalidCharacterExeption : public exception {
 public:
     const char* what() const noexcept override {
-        return "Invalid Character: The start character must be a letter (A-Z, a-z)"
+        return "Invalid Character: must be a letter (A-Z, a-z).";
     }
     
+};
+
+class invalidRangeExcpetion: public exception { public:
+    const char* what() const noexcept override {
+        return "Invalid range: not a letter.";
+    }
+};
+char character(char start, int offset) {
+    if (!((start >= 'A' && start <='Z') || (start >= 'a' && start <= 'z'))){
+        throw  invalidCharacterExeption();
+    }
+    char target = start + offset;
 }
