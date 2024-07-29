e65928f1c1797c2e1030706580eb87bad850f7be
branden <branden.bowling@sinclair.edu>
2024-07-28 22:50:32 -0400

    Initial Commit

---
+++ part a/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  part a
+//
+//  Created by Branden B on 7/28/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
